45710c089b136a1c310ecb23680397907ea3b939
oreolord <oribush26@gmail.com>
2025-03-27 19:22:08 -0400

    Revert because it didnt work

--- archinstall.sh
+++ archinstall.sh
@@ -9,19 +9,11 @@ do
     gpubrand=""
     cpubrand=""
     booter=""
-    bootpar=""
-    homepar=""
-    swappar=""
+
     if [[ "${drive:0:3}" == "nvm" ]]; then
         type="NVME"
-        bootpar = "/dev/$drive\p1"
-        swappar = "/dev/$drive\p2"
-        rootpar = "/dev/$drive\p3"
     else
         type="SATA"
-        bootpar = "/dev/$drive\1"
-        swappar = "/dev/$drive\2"
-        rootpar = "/dev/$drive\3"
     fi
     PS3="What is your CPU brand? "
     select optiona in "Intel" "AMD"; do
@@ -50,13 +42,13 @@ do
         esac
     done
     PS3="Pick a bootloader: "
-    select optionc in "Grub" "Systemd"; do
+    select optionc in "Grub (recommended)" "Systemd (faster)"; do
         case $optionc in
-            "Grub")
-                booter="Grub (recommended)"
+            "Grub (recommended)")
+                booter="Grub"
                 break
                 ;;
-            "Systemd")
+            "Systemd (faster)")
                 booter="Systemd"
                 break
                 ;;
@@ -102,13 +94,23 @@ echo
 echo
 echo w
 ) | fdisk /dev/$drive
-mkfs.fat -F32 $bootpar
-mkswap $swappar
-mkfs.ext4 $rootpar
-swapon $swappar
-mount $rootpar /mnt
-mkdir /mnt/boot
-mount $bootpar /mnt/boot
+if [ "$type" = "SATA" ]; then
+    mkfs.fat -F32 /dev/$drive\1
+    mkswap /dev/$drive\2
+    mkfs.ext4 /dev/$drive\3
+    swapon /dev/$drive\2
+    mount /dev/$drive\3 /mnt
+    mkdir /mnt/boot
+    mount /dev/$drive\1 /mnt/boot
+elif [ "$type" = "NVME" ]; then
+    mkfs.fat -F32 /dev/$drive\p1
+    mkswap /dev/$drive\p2
+    mkfs.ext4 /dev/$drive\p3
+    swapon /dev/$drive\p2
+    mount /dev/$drive\p3 /mnt
+    mkdir /mnt/boot
+    mount /dev/$drive\p1 /mnt/boot
+fi
 if [ "$cpubrand" = "Intel" ]; then
     pacstrap -K /mnt base base-devel linux linux-firmware git curl intel-ucode nano bash-completion networkmanager linux-headers
 elif [ "$cpubrand" = "AMD" ]; then
