95c6b8ad7c619e32451bf67cc39f9c53947833af
oreolord <oribush26@gmail.com>
2025-04-17 00:28:55 -0400

    Fix subprocess

--- __main__.py
+++ __main__.py
@@ -6,17 +6,17 @@ import subprocess
 
 flags = setup()
 drive = f"/dev/{flags[0]}"
-cpu = f"/dev/{flags[1]}"
-subprocess.run(["parted", drive, "mklabel", "gpt"], shell=True)
+cpu = flags[1]
+os.system(f"parted {drive} mklabel gpt")
 echo_sequence = "echo n\necho  \necho  \necho +512M\necho t\necho 1\necho  n\necho  \necho  \necho +8G\necho t\necho 2\necho 19\necho n\necho  \necho  \necho  \necho w"
-subprocess.run(f"({echo_sequence}) | fdisk {drive}", shell=True)
+os.system(f"({echo_sequence}) | fdisk {drive}")
 bootpar, swappar, rootpar = find_partitions(drive)
-subprocess.run(["mkfs.fat", "-F32", bootpar], shell=True)
-subprocess.run(["mkswap", swappar], shell=True)
-subprocess.run(["mkfs.ext4", rootpar], shell=True)
-subprocess.run(["swapon", swappar], shell=True)
-subprocess.run(["mount", rootpar, "/mnt"], shell=True)
-subprocess.run(["mount", "-m", bootpar, "/mnt/boot"], shell=True)
+os.system(f"mkfs.fat -F32 {bootpar}")
+os.system(f"mkswap {swappar}")
+os.system(f"mkfs.ext4 {rootpar}")
+os.system(f"swapon {swappar}")
+os.system(f"mount {rootpar} /mnt")
+os.system(f"mount -m {bootpar} /mnt/boot")
 os.system("reflector --verbose --country 'United States' -l 5 --sort rate --save /etc/pacman.d/mirrorlist")
 install_linux(cpu)
 os.system("genfstab -U /mnt >> /mnt/etc/fstab")
